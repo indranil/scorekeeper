2681959f2dc8de7fa9d9ba601989e1549437b9e2
Indranil Dasgupta <indranild@gmail.com>
2017-10-18 19:23:13 +0530

    Added enzyme and made sure it runs

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { mount, shallow } from 'enzyme';
 
 // Import all the components
 import App from '../components/App';
+import Header from '../components/Header';
 
 describe('smoke test', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
   });
-  it('loads default starting point', () => {
-    
+  it('enzyme installed', () => {
+    shallow(<App />);
+  });
+  it('loads the app', () => {
+    const app = mount(<App />);
+    const heading = <h1>Scorekeeper</h1>;
+    expect(app.contains(heading)).toEqual(true);
   });
 });

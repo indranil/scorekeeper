577fdbcf0698ab2508c97acf3ca5bf07191c76cf
Indranil Dasgupta <indranild@gmail.com>
2018-04-14 18:01:22 +0200

    put emoji within span as per style guide

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,7 @@ const Footer = () => (
   <footer id="footer" className="container">
     <div className="row">
       <div className="columns">
-        <p><em>A tiny app by <a href="https://github.com/indranil">Indranil Dasgupta</a></em> ✌🏽</p>
+        <p><em>A tiny app by <a href="https://github.com/indranil">Indranil Dasgupta</a></em><span role="img" aria-label="peace!">✌🏽</span></p>
       </div>
     </div>
   </footer>

1b5a9310a8b66da0848feeefb515235a10a38677
Indranil Dasgupta <indranild@gmail.com>
2018-04-14 17:58:36 +0200

    added surge build process to travis

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,7 @@ const Footer = () => (
   <footer id="footer" className="container">
     <div className="row">
       <div className="columns">
-        <p><em>A tiny app by <a href="https://github.com/indranil">Indranil Dasgupta</a></em></p>
+        <p><em>A tiny app by <a href="https://github.com/indranil">Indranil Dasgupta</a></em> ✌🏽</p>
       </div>
     </div>
   </footer>
